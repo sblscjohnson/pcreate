06f651c79436c9cd37bb1d619309d337530e0b0c
sblscjohnson <johnson.s.8253@gmail.com>
2019-02-13 15:33:19 -0700

    responsive

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,31 +1,76 @@
-import React from 'react';
+import React, {Component} from 'react';
 import pcreate_logo from './../../resources/images/pcreate_logo.png';
 import {Link} from 'react-router-dom';
 import routes from './../../routes';
-import './nav.css'
+import './../../resources/hamburgers.css'
+import './nav.css';
 
-const Nav = (props) => {
-  return(
-    <div>
-      <nav className='navbar'>
+
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      button_on: 'no'
+    };
+  }
+
+  buttonOn = () => {
+    this.setState({
+      button_on: 'yes'
+    })
+  }
+
+  buttonOff = () => {
+    this.setState({
+      button_on: 'no'
+    })
+  }
+
+  render() {
+  if(this.state.button_on === 'no') {
+    return(
+      <div>
+        <nav className='navbar_lines'>
+        <Link to='/'><img className='logo' src={pcreate_logo} alt='pcreate' /></Link>
+        <div className='small'>
+        <button className='lines' className='hamburger hamburger--vortex' onClick={this.buttonOn} type='button'>
+          <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+          </span>
+        </button>
+        </div>
+          <div className='normal_nav'>
+            <Link className='normal_item' to='/NewBuild'>New Build</Link>
+            <Link className='normal_item' to='/CompletedBuilds'>Completed Builds</Link>
+            <Link className='normal_item' to='/Account'>Account</Link>
+          </div>
+        </nav>
+        {routes}
+      </div>
+    )
+  } else {
+      return(
+        <div>
+      <nav className='navbar_x'>
         <Link to='/'><img className='logo' src={pcreate_logo} alt='pcreate' /></Link>
         <div className='dropdown'>
-          <button><i class="fas fa-bars"></i></button>
-          <div className='dropdownItems'>
-            <Link to='/NewBuild'>New Build</Link>
-            <Link to='/CompletedBuilds'>Completed Builds</Link>
-            <Link to='/Account'>Account</Link>
+        <button className='x' className='hamburger hamburger--vortex is-active' onClick={this.buttonOff} type='button'>
+          <span class="hamburger-box">
+            <span class="hamburger-inner"></span>
+          </span>
+        </button>
+          <div className='dropdownItems_notHidden'>
+            <Link className='dropdown_link' onClick={this.buttonOff} to='/NewBuild'>New Build</Link>
+            <Link className='dropdown_link' onClick={this.buttonOff} to='/CompletedBuilds'>Completed Builds</Link>
+            <Link className='dropdown_link' onClick={this.buttonOff} to='/Account'>Account</Link>
           </div>
         </div>
-        <div className='navItems'>
-          <Link to='/NewBuild'>New Build</Link>
-          <Link to='/CompletedBuilds'>Completed Builds</Link>
-          <Link to='/Account'>Account</Link>
-        </div>
+        
       </nav>
       {routes}
     </div>
-  )
+      )
+  }}
 }
 
 export default Nav
